a58535c833ddf14fa28efa22e9565725a5e4d1e1
gustavoreice <gustavo.hdosantos@gmail.com>
2025-01-27 19:04:40 +0000

    progresso no desafio

---
+++ NovatoCartasSuperTrunfoA01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+    int main(){
+        int pontoturistico = 15;
+        float populacao = 316.473;
+        float area = 64.17;
+        float pib = 58.027;
+        char nome[20] = "Barueri"; 
+
+       
+
+        printf("Nome da cidade: %s\n", nome);
+        printf("Número de habitante: %.3f\n", populacao);
+        printf("km²: %.2f\n", area);
+        printf("PIB em Bilhões: %.3f\n", pib);
+        printf("Número de pontos turísticos: %d\n", pontoturistico);
+
+        return 0;
+
+
+
+
+
+    }

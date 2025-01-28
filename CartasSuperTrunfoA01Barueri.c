c006316b0869ecd26ca7eef1661afdac998568fb
gustavoreice <gustavo.hdosantos@gmail.com>
2025-01-28 16:23:48 +0000

    alteração

--- CartasSuperTrunfoA01Barueri.c
+++ CartasSuperTrunfoA01Barueri.c
@@ -6,7 +6,7 @@
         int pontoturistico = 15;
         float populacao = 316.473;
         float area = 64.17;
-        float pib = 58.027;
+        float pib = 207.460;
         char nome[20] = "Barueri"; 
 
        
@@ -14,13 +14,27 @@
         printf("Nome da cidade: %s\n", nome);
         printf("Número de habitante: %.3f\n", populacao);
         printf("km²: %.2f\n", area);
-        printf("PIB em Bilhões: %.3f\n", pib);
+        printf("PIB per capita: %.3f\n", pib);
         printf("Número de pontos turísticos: %d\n", pontoturistico);
 
         return 0;
+        }
+
+        int main(){
+             /*Carta A02 referência cidade de Itapevi SP*/
+        int pontoturistico = 15;
+        float populacao = 232.297;
+        float area = 82.659;
+        float pib = 52.656;
+        char nome[20] = "Itapevi"; 
+
+       
+
+        printf("Nome da cidade: %s\n", nome);
+        printf("Número de habitante: %.3f\n", populacao);
+        printf("km²: %.3f\n", area);
+        printf("PIB per capita: %.3f\n", pib);
+        printf("Número de pontos turísticos: %d\n", pontoturistico);
 
 
-
-
-
-    }
+        }

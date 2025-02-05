77a3a8bf6a52d9a5619bdfca7e2097b029bd38c1
gustavoreice <gustavo.hdosantos@gmail.com>
2025-02-05 21:02:13 +0000

    mexendo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,6 +18,41 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    
+    //Introdução de variaveis:    
+        int pontoturistico, codigo;
+        float populacao ;
+        float area ;
+        float pib ;
+        char nome[20];  
+
+    //Entrada de dados:
+
+       printf("Qual o código da cidade resgistro no IBGE ?\n", codigo);
+       scanf("%d", &codigo);
+
+       printf("Qual o nome da cidade escolhida?\n", nome);
+       scanf("%s", &nome); 
+
+       printf("Qual o número de pontos turisticos da cidade escolhida?\n", pontoturistico);
+       scanf("%d", &pontoturistico);
+
+       printf("Qual o total de habitantes registrado no IBGE da cidade escolhida ?\n", populacao);
+       scanf("%f", &populacao);
+
+       printf("Qual a área em KM²?\n", area);
+       scanf("%f", &area);
+
+       printf("Qual o PIB da cidade registra no IBGE?\n", pib);
+       scanf("%f", &pib);
+
+        
+    //Saida de Dados Recebidos:
+        printf("Cidade: %s\n", nome);
+        printf("Código do Município: %d\n", codigo);
+        printf("Habitante: %.3f\n", populacao);
+        printf("Área: %.3fkm²\n", area);
+        printf("PIB per capita: %.3f R$\n", pib);
+        printf("Número de pontos turísticos: %d\n", pontoturistico);
+
     return 0;
 }

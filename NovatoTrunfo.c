1e174be6f7ce645fa1b6ce50e247f9ec4561a432
gustavoreice <gustavo.hdosantos@gmail.com>
2025-02-12 16:47:42 +0000

    correção

--- NovatoTrunfo.c
+++ NovatoTrunfo.c
@@ -9,15 +9,21 @@ int main() {
         float populacao ;
         float area ;
         float pib ;
-        char nome[20];  
+        char nome[20]; 
+        char estado[21];  
 
     //Entrada de dados:
+
+        
 
        printf("Qual o código da cidade resgistro no IBGE ?\n", codigo);
        scanf("%d", &codigo);
 
        printf("Qual o nome da cidade escolhida?\n", nome);
        scanf("%s", &nome); 
+
+       printf("Qual o nome da estado escolhido?\n", estado);
+       scanf("%s", &estado); 
 
        printf("Qual o número de pontos turisticos da cidade escolhida?\n", pontoturistico);
        scanf("%d", &pontoturistico);
@@ -33,12 +39,13 @@ int main() {
 
         
     //Saida de Dados Recebidos:
+
+        printf("Estado: %s\n", estado);
         printf("Cidade: %s\n", nome);
         printf("Código do Município: %d\n", codigo);
         printf("Habitante: %.3f\n", populacao);
         printf("Área: %.3fkm²\n", area);
-        printf("PIB : %.2f R$\n", pib);
+        printf("PIB : %.3f Milhões de Reais\n", pib);
         printf("Número de pontos turísticos: %d\n", pontoturistico);
 
     return 0;
-}

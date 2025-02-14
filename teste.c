7f724c2b1d42f8c0d5ef0dbb7d850c32fd8d9790
gustavoreice <gustavo.hdosantos@gmail.com>
2025-02-14 19:58:10 +0000

    teste

--- teste.c
+++ teste.c
@@ -4,17 +4,15 @@
 int main() {
     
     //Introdução de variaveis:    
-    
         int pontoturistico, codigo;
+        long int soma;
         float populacao ;
         float area ;
-        float pib ;
-        char nome[20]; 
-        char estado[21];  
+        float pib, densidade, capita ;
+        char nome[30];
+        char estado[30];  
 
     //Entrada de dados:
-
-        
 
        printf("Qual o código da cidade resgistro no IBGE ?\n", codigo);
        scanf("%d", &codigo);
@@ -22,7 +20,7 @@ int main() {
        printf("Qual o nome da cidade escolhida?\n", nome);
        scanf("%s", &nome); 
 
-       printf("Qual o nome da estado escolhido?\n", estado);
+       printf("Qual a sigla do estado da cidade escolhido.+?\n", estado);
        scanf("%s", &estado); 
 
        printf("Qual o número de pontos turisticos da cidade escolhida?\n", pontoturistico);
@@ -37,15 +35,29 @@ int main() {
        printf("Qual o PIB da cidade registra no IBGE?\n", pib);
        scanf("%f", &pib);
 
+    //Introdução de calculos:
+
+       soma = pontoturistico + populacao + area + pib ;
+
+       densidade = populacao / area ;
+
+       capita = pib / populacao ;   
+
         
     //Saida de Dados Recebidos:
-
         printf("Estado: %s\n", estado);
         printf("Cidade: %s\n", nome);
         printf("Código do Município: %d\n", codigo);
         printf("Habitante: %.3f\n", populacao);
         printf("Área: %.3fkm²\n", area);
-        printf("PIB : %.3f Milhões de Reais\n", pib);
+        printf("PIB : %.2f Milhões de Reais\n", pib);
         printf("Número de pontos turísticos: %d\n", pontoturistico);
+    
+    // Saida de Calculos:
+
+        printf("Densidade Populacional: %.3f por KM²\n", densidade);
+        printf("PIB per Capita: %.2f R$\n", capita);
+        printf("Super poder: %d", soma);
 
     return 0;
+}

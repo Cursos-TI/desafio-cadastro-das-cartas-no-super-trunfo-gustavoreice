4c6759fb9046ed74917bbc194cd5f52de73b431b
gustavoreice <gustavo.hdosantos@gmail.com>
2025-02-20 17:54:00 +0000

    finalizado

--- IntermediarioTrunfo.c
+++ IntermediarioTrunfo.c
@@ -5,11 +5,9 @@ int main() {
     
     //Introdução de variaveis:    
         int pontoturistico, codigo;
-        float populacao ;
-        float area ;
-        float pib, densidade, capita ;
-        char nome[30];
-        char estado[30];  
+        float populacao, area, pib, densidade, capita ;
+        char nome[30], estado[30];
+        
 
     //Entrada de dados:
 
@@ -19,7 +17,7 @@ int main() {
        printf("Qual o nome da cidade escolhida?\n", nome);
        scanf("%s", &nome); 
 
-       printf("Qual a sigla do estado da cidade escolhido.+?\n", estado);
+       printf("Qual a sigla do estado da cidade escolhido?\n", estado);
        scanf("%s", &estado); 
 
        printf("Qual o número de pontos turisticos da cidade escolhida?\n", pontoturistico);
@@ -47,13 +45,13 @@ int main() {
         printf("Código do Município: %d\n", codigo);
         printf("Habitante: %.3f\n", populacao);
         printf("Área: %.3fkm²\n", area);
-        printf("PIB : %.2f Milhões de Reais\n", pib);
+        printf("PIB : %.2f R$\n", pib);
         printf("Número de pontos turísticos: %d\n", pontoturistico);
     
     // Saida de Calculos:
 
         printf("Densidade Populacional: %.3f por KM²\n", densidade);
-        printf("PIB per Capita: %.2f Milhões de Reais\n", capita);
+        printf("PIB per Capita: %.2f R$\n", capita);
 
     return 0;
 }
